f0f195c0fd5e8ba16722e161b92011fa1282c00d
Felipe Garcia <felipe@dnagata.com.br>
2025-09-19 08:15:06 -0300

    Aumento pool conexões

--- server/db.ts
+++ server/db.ts
@@ -29,9 +29,9 @@ async function createDbWithTransport(mode: TransportMode) {
   setTransport(mode);
   const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    max: 10,
+    max: 45,
     idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+    connectionTimeoutMillis: 3000,
     allowExitOnIdle: true
   });
   const db = drizzle({ client: pool, schema });
