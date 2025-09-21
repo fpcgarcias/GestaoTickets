e6369a11e9735648ea013e3de5888a2190656aba
Felipe Garcia <felipe@dnagata.com.br>
2025-09-21 12:48:11 -0300

    Correção opentelemetry

--- server/telemetry/otel-config.ts
+++ server/telemetry/otel-config.ts
@@ -4,7 +4,7 @@
  */
 
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/instrumentation';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
